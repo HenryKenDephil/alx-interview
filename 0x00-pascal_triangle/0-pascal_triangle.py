133c23620214820500c4d5945a6186df559b891a
HenryKenDephil <h.komoloh@gmail.com>
2023-01-27 01:10:02 +0300

    code refactor

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-
 def pascal_triangle(n):
-    if n <= 0:
-        return []
+  """draws a pascal triangle
+  Args:
+      n (n): size of triangle
+  Returns:
+      list: returns empty list if n <= 0 or list of lists of integers
+      representing Pascal's triangle of n otherwise
+  """
+  if n <= 0:
+    return []
+  
+  pascal_triangle, buffer_list = [], []
+  for x in range(n):
+    if x == 0:
+      pascal_triangle.append([1])
+      continue
+    if x == 1:
+      pascal_triangle.append([1, 1])
+      continue
     
-    for i in range(1, n+1):
-        for j in range(0, n-i+1):
-            print(" ", end=" ")
-            
-        #assign first element e to be 1
-        e = 1
-        for j in range(1, i+1):
-
-            #always expect the first value in the line to be 1
-            
-            print(" ", e, sep=" ", end=" ")
-
-            #apply the concept of Bionomial Coefficients
-
-            e = e *(i-j)//j
-        print()
-
-
-
-pascal_triangle(n)
+    sum_list = pascal_triangle[-1]
+    for i in range(len(sum_list) + 1):
+      if i in [0, len(sum_list)]:
+        buffer_list.append(1)
+        continue
+      buffer_list.append(sum_list[i] + sum_list[i - 1])
+    
+    pascal_triangle.append(buffer_list)
+    buffer_list = []
+    
+  return pascal_triangle
