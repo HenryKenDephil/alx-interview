244d7547b6c2d9e07851e4fa90745c809565bd7d
HenryKenDephil <h.komoloh@gmail.com>
2023-02-01 22:45:45 +0300

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-#python program that draws a pascal triangle
-#the program will return a list of integers representing pascal triangle of size n
-def pascal_triangle(n):
-  '''a function to that draws a psacal triangle
+# python program that draws a pascal triangle
+# the program will return a list of integers representing pascal triangle of size n
+#def pascal_triangle(n):
+'''a function to that draws a psacal triangle
       Returns:
             a list of integers representing pascal triangle of n
             an empty list if n <= 0
-      Assumes that n is always an integer 
+      Assumes that n is always an integer
       Args:
             n (n): size of triangle
-  '''
-  
+
+
   #check if n is an integer
   if type(n) is not int:
     raise TypeError("n must be an integer")
@@ -20,17 +20,17 @@ def pascal_triangle(n):
     raise ValueError("n must be greater than 0")
 
  #set the current list
-  previous = []
+  previous = [[1]]
 
  #generate the pattern list
-  for i in range(n):
+  for i in range(1,n):
     temp_list = []
-    for j in range(i + 1):
+    for j in range(1, i):
       if j == 0 or j== i:
         temp_list.append(1)
 
       else:
-        temp_list.append(previous[i - 1][j - 1] + previous[i - 1][j]) 
+        temp_list.append(previous[i - 1][j - 1] + previous[i - 1][j])
 
     previous.append(temp_list)
 
@@ -44,3 +44,23 @@ def pascal_triangle(n):
 
     print()
 
+'''
+def pascal_triangle(n):
+    '''a function to that draws a psacal triangle
+        Returns:
+              a list of integers representing pascal triangle of n
+              an empty list if n <= 0
+        Assumes that n is always an integer
+        Args:
+              n (n): size of triangle
+    '''
+    if n <= 0:
+      return []
+      triangle = [[1]]
+      for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            row.append(1)
+            triangle.append(row)
+      return triangle
