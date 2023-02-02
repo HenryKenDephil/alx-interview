a0dd951a5948cbb7757d2ecc0ac21000ebc0e141
HenryKenDephil <h.komoloh@gmail.com>
2023-02-02 23:54:30 +0300

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-#a python program to determine if the closed boxes can be openned
-#the closed boxes should be openned using keys in random boxes
+
+""" Lockboxes """
+
 
 def canUnlockAll(boxes):
-    '''
-    a function to determine if the available keys can unlock the boxes all boxes.
-    the boxes are in form of list of lists
-    a key with same number as a box will open the box
-    some keys might not open some boxes
-    '''
+    """
+    - boxes is a list of lists
+    - A key with the same number as the box will open that box
+    - assuming all keys to be positive integers
+    - The first box boxes[0] is unlocked
+    - Return True if all boxes can be opened, else return False
+    """
+    canUnlockAll = False
+    keys = {0: True}
     n = len(boxes)
-    keys = []
-    if n<=0: raise ValueError('n must be greater than 0')
-    if type(n) is not int: raise typeError('n must be an integer')
-#first box is unlocked
-    open_boxes = []
+    while (True):
 
-    if open_boxes:
-        return True
-    if boxes != open_boxes:
-        return False
+        n_keys = len(keys)
 
-    while True:
-        for i in range(1, n + 1):
-            if boxes[i] == keys[i]:
-                open_boxes.append([1]*(i + 1))
+        for i in range(n):
+            if boxes[i] and keys.get(i, False):
+                for j in boxes[i]:
+                    if j < n:
+                        keys[j] = True
+                    boxes[i] = None
 
-            for j in range(1, i + 1):
-                if j == 0 or j == i:
-                    return open_boxes.append([1-j][j-1])
+        if not (len(keys) > n_keys):
+            break
 
-        return open_boxes.append([i-1][j-1] + open_boxes[i][j])
-    
+    if n_keys == len(boxes):
+        canUnlockAll = True
 
-    return canUnlockAll;
-    
+    return canUnlockAll
