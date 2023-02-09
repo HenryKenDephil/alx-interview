7060e39cf9bf31bfeee922318dd1473e85b70a18
HenryKenDePhil <h.komoloh@gmail.com>
2023-02-10 01:28:15 +0300

    minimum operations to move through an array

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''
+    minimum operations in an array
+'''
+
+
+def minOperations(n):
+    '''
+        a function to calculate the minimu number of operations
+        to result in exactly n H characters in the given file
+
+        Args:
+            n: number of operations
+            fewer_operations: number of minimum operations
+            possible_operations: number of achievable operations
+        Returns: return the minimu number of operations
+                : returns 0 if n is impossible
+    '''
+
+    if n < 2 or type(n) is not int:
+        return 0
+    count = 1
+    arr = list()
+    operations = n
+    while operations != 1:
+        count += 1
+        if operations % count == 0:
+            while operations % count == 0 and operations != 1:
+                operations /= count
+                arr.append(count)
+
+    return sum(arr)
+
+#minOperations(4)
+
+
+
+
